a022b909eb044ca27d7e4c015649278a95b64cd7
Mubashir Imtiaz <mubashir.imtiaz123@gmail.com>
2020-07-25 14:12:58 +0500

    Added Loader and Dragging effect

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -30,29 +30,32 @@ const Header = () => {
   };
   return (
     <header>
-      <Link to="/">
-        <div className="logo">
-          <motion.svg
-            variants={svgVariants}
-            initial="hidden"
-            animate="visible"
-            className="pizza-svg"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 100"
-          >
-            <motion.path
-              variants={pathVariants}
-              fill="none"
-              d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-            />
-            <motion.path
-              variants={pathVariants}
-              fill="none"
-              d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
-            />
-          </motion.svg>
-        </div>
-      </Link>
+      <motion.div
+        className="logo"
+        drag
+        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+        dragElastic={1}
+      >
+        <motion.svg
+          variants={svgVariants}
+          initial="hidden"
+          animate="visible"
+          className="pizza-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+        >
+          <motion.path
+            variants={pathVariants}
+            fill="none"
+            d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+          />
+          <motion.path
+            variants={pathVariants}
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+          />
+        </motion.svg>
+      </motion.div>
       <motion.div
         className="title"
         initial={{ y: -250 }}
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import Loader from "../loader/Loader";
 const Home = () => {
   const containerVariants = {
     hidden: {
@@ -45,6 +46,7 @@ const Home = () => {
           Create Your Pizza
         </motion.button>
       </Link>
+      <Loader />
     </motion.div>
   );
 };
